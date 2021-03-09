f2690a759c4502b0000787e37fa317d37edc7735
Gustavo Moreno <gustavo@bookmymusic.fr>
2021-03-09 20:18:05 +0100

    Handle errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React, { RefObject, KeyboardEvent } from 'react';
+import React, { RefObject, KeyboardEvent, ReactElement } from 'react';
 import './App.scss';
 import { Character } from './models';
 import { CharactersService } from './services';
 import { CharacterView } from './characters';
+
+const errors = {
+  timeout: 'Request couldn\'t be completed. Please try again.'
+}
 
 class App extends React.Component<{}, {
   searchTerm: string,
@@ -11,6 +15,7 @@ class App extends React.Component<{}, {
   searchOffset?: number,
   loading: boolean,
   noResults: boolean,
+  error?: string,
 }> {
   searchInputRef: RefObject<HTMLInputElement>;
 
@@ -62,7 +67,13 @@ class App extends React.Component<{}, {
         characters: result.results,
         showFetchMoreResults: result.offset + result.results.length < result.total,
         searchOffset: result.results.length,
-        loading: false
+        loading: false,
+        error: undefined,
+      });
+    }, () => {
+      this.setState({
+        loading: false,
+        error: errors.timeout
       });
     });
   }
@@ -76,25 +87,50 @@ class App extends React.Component<{}, {
         characters: [...this.state.characters, ...result.results],
         showFetchMoreResults: result.offset + result.results.length < result.total,
         searchOffset: this.state.characters.length + result.results.length,
+        loading: false,
+        error: undefined
+      });
+    }, () => {
+      this.setState({
+        error: errors.timeout,
         loading: false
       });
     });
   }
 
   render() {
-    return (
-      <div className="container mt-3">
-        <input type="text" className="d-block mx-auto"
-          ref={this.searchInputRef}
-          onChange={this.handleSearchInputChange}
-          onKeyPress={this.handleKeyPressOnSearchInput}/>
+    const renderError = (): ReactElement | undefined => {
+      if ( ! this.state.error ) {
+        return;
+      }
+      return (
+        <div className="alert alert-danger mt-2">
+          {this.state.error}
+        </div>
+      )
+    }
 
-        {!this.state.noResults? <div className="results mx-auto my-3">
+    const renderResults = (): ReactElement | undefined => {
+      if ( this.state.noResults ) {
+        return (
+          <div className="alert alert-primary mt-2" role="alert">
+            No results were found
+          </div>
+        );
+      }
+
+      if ( this.state.error && ! this.state.characters.length ) {
+        // Search request failed
+        return renderError();
+      }
+
+      return (
+        <div className="results mx-auto my-3">
           <div className="row">
             {this.state.characters.map(character => {
               return (
-                <div className="col-sm-6 mb-1">
-                  <CharacterView key={character.id} character={character}></CharacterView>
+                <div key={character.id} className="col-sm-6 mb-1">
+                  <CharacterView character={character}></CharacterView>
                 </div>
               );
             })}
@@ -104,14 +140,24 @@ class App extends React.Component<{}, {
             {this.state.loading?
             <div className="spinner-border" role="status">
               <span className="sr-only">Loading...</span>
-            </div> : this.state.showFetchMoreResults &&
-            <button className="btn btn-primary" onClick={this.handleClickFetchMoreResults}>
+            </div> : this.state.showFetchMoreResults && [
+              this.state.error && renderError(),
+              <button className="btn btn-primary" onClick={this.handleClickFetchMoreResults}>
                 More results
-            </button>}
+              </button>]
+            }
           </div>
-        </div> : <div className="alert alert-primary mt-2" role="alert">
-          No results were found
-        </div>}
+        </div>
+      )
+    }
+
+    return (
+      <div className="container mt-3">
+        <input type="text" className="d-block mx-auto"
+          ref={this.searchInputRef}
+          onChange={this.handleSearchInputChange}
+          onKeyPress={this.handleKeyPressOnSearchInput}/>
+        {renderResults()}
       </div>
     );
   }
